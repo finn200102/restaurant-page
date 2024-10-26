16c66106acfaa7c396e6a7244bc7fe0dee6a589f
Finn <112250336+finn200102@users.noreply.github.com>
2024-10-26 12:54:18 +0200

    Add method for homebutton

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "./styles.css";
 import { homepage } from "./homepage.js";
+import { menuPage } from "./menue.js";
+import { aboutPage } from "./about.js";
 
-homepage.renderHomepage();
+//homepage.renderHomepage();
+//menuPage.renderMenuPage();
+//aboutPage.renderAboutPage();
+
+const homeButton = document.getElementById("home-button");
+homeButton.addEventListener("click", function () {
+  homepage.renderHomepage();
+});
